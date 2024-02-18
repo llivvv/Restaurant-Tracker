322cc12cec02bd8180f8b9fa2a5af849a0dd1c66
Livia L <llin24@students.cs.ubc.ca>
2024-02-18 12:52:58 -0800

    Added tests for the food class

--- src/main/model/RestaurantRatingComparator.java
+++ src/main/model/RestaurantRatingComparator.java
@@ -5,6 +5,7 @@ package model;
 // https://www.freecodecamp.org/news/how-to-sort-a-list-in-java/#:~:text=To%20do%20this%2C%20you%20will,integer%20indicating%20their%20relative%20order.
 public class RestaurantRatingComparator implements java.util.Comparator<Restaurant> {
 
+    // EFFECTS: compares 2 restaurants
     @Override
     public int compare(Restaurant a, Restaurant b) {
         return (int) (a.getRating() - b.getRating());
--- src/test/model/FoodTest.java
+++ src/test/model/FoodTest.java
@@ -1,4 +1,55 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FoodTest {
+
+    private Food testfoodtried;
+    private Food testfoodwish;
+
+    @BeforeEach
+    public void runBefore() {
+        testfoodtried = new Food("tea", 7, true);
+        testfoodwish = new Food("melon", 2, false);
+    }
+
+    @Test
+    public void setRatingWishTest() {
+        testfoodwish.setRating(2);
+        assertEquals(0, testfoodwish.getRating());
+    }
+
+    @Test
+    public void setRatingTriedTest() {
+        testfoodtried.setRating(3);
+        assertEquals(3, testfoodtried.getRating());
+    }
+
+    @Test
+    public void setRatingTried0Test() {
+        testfoodtried.setRating(0);
+        assertEquals(0, testfoodtried.getRating());
+    }
+
+    @Test
+    public void setRatingTried5Test() {
+        testfoodtried.setRating(5);
+        assertEquals(5, testfoodtried.getRating());
+    }
+
+    @Test
+    public void setRatingTried6Test() {
+        testfoodtried.setRating(6);
+        assertEquals(5, testfoodtried.getRating());
+    }
+
+    @Test
+    public void makeTriedTest() {
+        assertFalse(testfoodwish.getisTried());
+        testfoodwish.makeTried();
+        assertTrue(testfoodwish.getisTried());
+    }
 }
