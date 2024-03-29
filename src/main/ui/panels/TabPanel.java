dc34d7ad0c254b6695938547e24af58a637641d7
Livia L <llin24@students.cs.ubc.ca>
2024-03-29 02:05:39 -0700

    added specifications for the TabPanel class

--- src/main/ui/panels/TabPanel.java
+++ src/main/ui/panels/TabPanel.java
@@ -5,37 +5,36 @@ import ui.ReviewTrackerGUI;
 import javax.swing.*;
 import java.awt.*;
 
-// Represents a panel having a button to create a new review and returning to homepage
+// Represents a panel having a button to create a new review
+
 public class TabPanel extends Panel {
 
     private JButton btnCreate;
-    private JButton btnHome;
     private ReviewTrackerGUI app;
 
+    // MODIFIES: this
+    // EFFECTS: constructs panel with button added
     public TabPanel(ReviewTrackerGUI app) {
 
         this.app = app;
         btnCreate = new JButton(" + ");
-        btnHome = new JButton(" Home ");
 
         configureButtons();
         add(btnCreate);
-        //add(btnHome);
 
         setBackground(new Color(161, 119, 100));
     }
 
+    // MODIFIES: this
+    // EFFECTS: adds action listener and sets this button's preferred size
     public void configureButtons() {
         btnCreate.addActionListener(app);
         btnCreate.setPreferredSize(new Dimension(60, 20));
-        //btnHome.addActionListener(app);
     }
 
+    // EFFECTS: returns btnCreate
     public JButton getBtnCreate() {
         return btnCreate;
     }
 
-//    public JButton getBtnHome() {
-//        return btnHome;
-//    }
 }
